0e0e43b3033d6efa1109a8962d629dd354f8d3f0
sunneepark <hl2ozs@naver.com>
2020-06-02 23:56:38 +0900

    20/06/02 upload 크루스칼 알고리즘 *longlong변경

--- algorithm/algorithm/hanaro.cpp
+++ algorithm/algorithm/hanaro.cpp
@@ -1,21 +1,22 @@
 #include<iostream>
 #include <vector>
 #include<algorithm>
+#include <math.h>
 using namespace std;
 
 int *visited;
 class ptp {
-public :
-	int firstPoint;
-	int secondPoint;
+public:
+	long long firstPoint;
+	long long secondPoint;
 	long long distance;
-public : ptp() {
-	}
-public : ptp(int f, int s, long long d) {
-		firstPoint = f; secondPoint = s; distance = d;
-	}
+public: ptp() {
+}
+public: ptp(long long f, long long s, long long d) {
+	firstPoint = f; secondPoint = s; distance = d;
+}
 };
-long long calL(int x1, int x2, int y1, int y2) {
+long long calL(long long x1, long long x2, long long y1, long long y2) {
 	long long disX = (x1 - x2)*(x1 - x2);
 	long long disY = (y1 - y2)*(y1 - y2);
 	return disX + disY;
@@ -48,45 +49,47 @@ int main(int argc, char** argv)
 {
 	int test_case;
 	int T;
-	
+
 	cin >> T;
 	for (test_case = 1; test_case <= T; ++test_case)
 	{
 		//input 받기
 		int n = 0;
 		cin >> n;
-		//vector<pair<long long, long long>> 
-		pair<long long, long long > *point = new pair<long long, long long>[n];
+		vector<pair<long long, long long>> point(n);
 		for (int i = 0; i < n; i++) {
-			cin >> point[i].first >> point[i].second;
+			cin >> point[i].first;
 		}
-		long long e =0.0;
+		for (int i = 0; i < n; i++) {
+			cin >> point[i].second;
+		}
+		double e = 0.0;
 		cin >> e;
-		cin.ignore();
+
 		visited = new int[n];
 		for (int i = 0; i < n; i++)
 			visited[i] = i;
 
 		vector<ptp> map;
-		for (int i = 0; i < n-1; i++) {
-			for (int j = i+1; j < n; j++) {
-				long long dis = e * calL(point[i].first, point[j].first, point[i].second, point[j].second);
+		for (int i = 0; i < n; i++) {
+			for (int j = i + 1; j < n; j++) {
+				long long dis = calL(point[i].first, point[j].first, point[i].second, point[j].second);
 				ptp temp(i, j, dis);
 				map.push_back(temp);
 			}
 		}
 		sort(map.begin(), map.end(), compare); //오름차순 정렬
-		int sum = 0;
-		for (int i = 0; i < map.size();i++) {
-			int first = map.at(i).firstPoint;
-			int second = map.at(i).secondPoint;
+		long long sum = 0;
+		for (int i = 0; i < map.size(); i++) {
+			long long first = map.at(i).firstPoint;
+			long long second = map.at(i).secondPoint;
 			if (!SameParent(first, second)) {
 				Union(first, second);
 				sum += map.at(i).distance;
 			}
 		}
 		map.clear();
-		cout << "#" << test_case << " " << sum << endl;
+		cout << "#" << test_case << " " << (long long)round((double)(e*sum)) << endl;
 	}
 	return 0;//정상종료시 반드시 0을 리턴해야합니다.
 }
