07d204daf866dc22cde08d76d3b5663ce4e3391e
sunneepark <hl2ozs@naver.com>
2020-06-04 00:11:25 +0900

    20/06/04 fix:최대비교

--- algorithm/algorithm/mathSuccessor.cpp
+++ algorithm/algorithm/mathSuccessor.cpp
@@ -7,7 +7,9 @@ using namespace std;
 vector<int> solution(vector<int> answers) {
 	vector<int> answer;
 	int sumS[3] = { 0 }; int sumIdx = -1;
+	int two[4] = { 1,3,4,5 };
 	int three[5] = { 3,1,2,4,5 };
+	int twoIdx = -1;
 	int threeIdx = -1;
 	for (int i = 0; i < answers.size(); i++) {
 		int answerTemp = answers.at(i);
@@ -20,22 +22,23 @@ vector<int> solution(vector<int> answers) {
 			if (2 == answerTemp) sumS[sumIdx]++;
 			threeIdx = ++threeIdx % 5;
 		}
-		else {			
-			if (((i-1) + (i / 2)) % 5 == answerTemp) sumS[sumIdx]++;
+		else {
+			twoIdx = (++twoIdx) % 4;
+			if (two[twoIdx] == answerTemp) sumS[sumIdx]++;
 		}
 
 		++sumIdx;
 		if (three[threeIdx] == answerTemp) sumS[sumIdx]++;
 	}
 	answer.push_back(1);
-	if (sumS[answer.back()-1] < sumS[1]) answer.pop_back();
-	else if (sumS[answer.back() - 1] < sumS[1]) answer.push_back(2);
+	if (sumS[answer.back() - 1] < sumS[1]) answer.pop_back();
+	if (answer.empty() || sumS[answer.back() - 1] == sumS[1]) answer.push_back(2);
 
 	while (!answer.empty() && sumS[answer.back() - 1] < sumS[2]) answer.pop_back();
-	if (sumS[answer.back() - 1] < sumS[2]) answer.push_back(3);
+	if (answer.empty() || sumS[answer.back() - 1] == sumS[2]) answer.push_back(3);
 	return answer;
 }
 
 int main() {
-	solution({ 1,3,2,4,2 });
+	solution({ 1,2,3,4,5,3,1,2,4,5 });
 }
