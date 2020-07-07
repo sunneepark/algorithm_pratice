86952e4841fa3042266b46b5d58f6404d03309e9
sunneepark <hl2ozs@naver.com>
2020-07-08 00:15:42 +0900

    20/07/07 fixed: 순열의 중복제거

--- algorithm/algorithm/findPrime.cpp
+++ algorithm/algorithm/findPrime.cpp
@@ -1,5 +1,6 @@
 #include <string>
 #include <vector>
+#include <set>
 #include <iostream>
 /**
 	@author sunny park
@@ -8,7 +9,7 @@
 using namespace std;
 
 string input = "";
-int answer = 0;
+set<string> combInputs; //중복제거
 
 bool isPrime(int num) {
 	if (num == 0 || num == 1) return false;
@@ -19,8 +20,7 @@ bool isPrime(int num) {
 }
 bool dfs(string cur, int curIdx, vector<bool> visited) {
 	cur += to_string(input[curIdx]-'0');
-
-	if (isPrime(stoi(cur))) answer++;
+	combInputs.insert(cur);
 	visited[curIdx] = true;
 
 	if (cur.size() == input.size()) return true;
@@ -32,6 +32,8 @@ bool dfs(string cur, int curIdx, vector<bool> visited) {
 
 int solution(string numbers) {
 	input = numbers;
+	int answer = 0;
+
 	bool isExistNum[10] = { false };
 	for (int i = 0; i < input.size(); i++) {
 		vector<bool> visited(input.size());
@@ -39,6 +41,8 @@ int solution(string numbers) {
 		if (num != 0 && !isExistNum[num]) dfs("",i, visited);
 		isExistNum[num] = true;
 	}
+	for (string temp : combInputs)
+		if (isPrime(stoi(temp))) answer++;
 	return answer;
 }
 int main() {
